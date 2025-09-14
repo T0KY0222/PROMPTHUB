b7bfc883efc2f9bb1a818f8e513ede990d54cfbe
Murat Bobr <doguzhaevm@gmail.com>
2025-09-15 01:28:28 +0300

    Обновлен favicon на новый логотип Prompt Raise

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,26 @@
 import '../styles.css'
 import '@solana/wallet-adapter-react-ui/styles.css'
 import React from 'react'
+import Head from 'next/head'
 import Layout from '../components/AppLayout.js'
 import WalletProviders from '../components/WalletProviders.js'
 
 export default function App({ Component, pageProps }) {
   return (
-    <WalletProviders>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </WalletProviders>
+    <>
+      <Head>
+        <title>Prompt Raise - Raise your use of AI</title>
+        <meta name="description" content="Discover and purchase premium AI prompts for ChatGPT, Claude, and more" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <WalletProviders>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </WalletProviders>
+    </>
   )
 }
